7b6788f03f256e34537cb9e65586a0805074a5e3
daveh <34u6SCktqUAq>
2020-11-19 15:14:01 -0600

    Updated rules to add topics, started dashboard

--- src/types.ts
+++ src/types.ts
@@ -6,47 +6,132 @@ export interface AppState extends ReduxState {
     theme: ThemeName;
 }
 
-export interface Category extends Record {
+export interface LessonPage {
+    title: string,
+    text: string,
+    photo?: string,
+    video?: string,
+    photoCaption?: string,
+    videoCaption?: string,
+    attestation?: string
+}
+
+export interface Question {
+    questionType: string;
+    questionText: string;
+    choices?: Array<{value: string}>;
+    correctAnswer: string|number;
+    explanation?: string;
+}
+
+export interface Answer {
+    question: string;
+    answerBefore?: string | number;
+    answerAfter?: string | number;
+    correctAnswer?: string | number;
+}
+
+export interface Community {
+    name: string;
+    intercomCompany?: string;
+}
+
+export interface GroupType {
     name: string;
 }
 
-export interface Product extends Record {
-    category_id: Identifier;
+export interface PageView {
+    attestationChecked?: boolean;
+    videoWatched?: boolean;
+}
+
+export interface LessonProgress {
+    id: Identifier;
+    lessonId: Identifier;
+    started?: Date;
+    completed?: Date;
+    preScore?: number;
+    score?: number;
+    answers: Array<Answer>;
+    pageViews: Array<PageView>;
+}
+
+export interface LessonProgressHash {
+    [lessonId:string] : LessonProgress
+}
+
+export interface User extends Record {
+    name: string;
+    email?: string;
+    phoneNumber?: string;
+    photoURL?: string;
+    organizationId?: Identifier;
+    organization?: string; // If not from the predefined list
+    community?: string;
+    acceptedTerms?: boolean;
+    canTeach?: boolean;
+    notificationsOn?: boolean;
+    intercomCompany?: object;
+    intercomTag?: object;
+}
+
+export interface Admin extends Record {
+    isAdmin?: boolean;
+    isClientAdmin?: boolean;
+    organizations?: string[];
+}
+
+export interface Organization extends Record {
+    name: string;
+    password?: string;
+    contactName?: string;
+    contactEmail?: string;
+    communities: Community[];
+    intercomCompany?: string;
+    intercomTag?: string;
+}
+
+export interface Lesson extends Record {
+    organizationId?: Identifier;
+    locale?: string;
+    name: string;
     description: string;
-    height: number;
-    image: string;
-    price: number;
-    reference: string;
-    stock: number;
-    thumbnail: string;
-    width: number;
+    photo: string;
+    pages: LessonPage[]; // Embedded list
+    questions: Question[]; // Embedded list
 }
 
-export interface Customer extends Record {
-    first_name: string;
-    last_name: string;
-    address: string;
-    city: string;
-    zipcode: string;
-    avatar: string;
-    birthday: string;
-    first_seen: string;
-    last_seen: string;
-    has_ordered: boolean;
-    latest_purchase: string;
-    has_newsletter: boolean;
-    groups: string[];
-    nb_commands: number;
-    total_spent: number;
+export interface Topic extends Record {
+    name: string;
+    description: string;
+    photo: string;
 }
 
-export interface Order extends Record {
-    basket: BasketItem[];
+export interface Subject extends Record {
+    topicId?: Identifier;
+    organizationId?: Identifier;
+    locale?: string;
+    name: string;
+    description: string;
+    photo: string;
+    lessons: Array<{lessonId: string}>; // Ordered list of lesson ids
+    groupTypes?: GroupType[];
 }
 
-export interface BasketItem {
-    product_id: string;
-    quantity: number;
+export interface TrainingSession extends Record {
+    name?: string;
+    archived: boolean;
+    userId: Identifier;
+    organizationId?: Identifier;
+    organization?: string;
+    community?: string;
+    subjectId: Identifier;
+    groupType?: string;
+    groupSize?: string;
+    groupSizeNum?: number;
+    started?: Date;
+    completed?: Date;
+    lessons?: LessonProgressHash;
 }
 
 /**
