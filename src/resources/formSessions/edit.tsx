3352ca489f63610539e63475c43e7d9e8c97679f
Oluebube Princess Egbuna <egbunaoluebube@gmail.com>
2021-10-12 12:52:10 +0100

    Build custom input for form answers

--- src/resources/formSessions/edit.tsx
+++ src/resources/formSessions/edit.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {
     BooleanInput,
     DateTimeInput,
@@ -15,6 +15,7 @@ import CustomEditToolbar from "../../components/CustomEditToolbar";
 import { ArrayInput } from "react-admin";
 import { OrderedFormIterator } from "../../components/OrderedFormIterator";
 import { PhotoInput } from "../../components/PhotoInput";
+import { FormDataConsumer, SimpleFormIterator } from "react-admin";
 
 const SessionEdit = (props: any) => {
 
@@ -44,6 +45,67 @@ const SessionEdit = (props: any) => {
                             <TextInput source="questionText" fullWidth={true} label="Question"/>
                             <TextInput source="questionType" fullWidth={true} label="Question Type" />
                             <BooleanInput source="isRequired" fullwidth={true} label="isRequired"/>
+                            <FormDataConsumer {...props}>
+                                {({ scopedFormData, getSource }:any) => {
+                                    console.log({ scopedFormData, getSource })
+                                    if (scopedFormData && scopedFormData.questionType) {
+                                        if (scopedFormData.questionType === 'multi-select') {
+                                            scopedFormData.answer = Object.values(scopedFormData.answer)
+                                        return (
+                                            <Fragment>
+                                                <ArrayInput source={getSource('choices')} label={`Possible choices for: ${scopedFormData.questionText}`}>
+                                                    <SimpleFormIterator disableAdd={true} disableRemove={true}>
+                                                        <TextInput source="value" label="Choice Value"
+                                                                    fullWidth={true}/>
+
+                                                    </SimpleFormIterator>
+                                                </ArrayInput>
+                                            </Fragment>);
+                                        } else if (scopedFormData.questionType === 'choose-one') {
+                                            return (
+                                                <Fragment>
+                                                    <ArrayInput source={getSource('choices')} label={`Possible choices for: ${scopedFormData.questionText}`}>
+                                                        <SimpleFormIterator disableAdd={true} disableRemove={true}>
+                                                            <TextInput source="value" label="Choice Value"
+                                                                       fullWidth={true}/>
+                                                        </SimpleFormIterator>
+                                                    </ArrayInput>
+                                                </Fragment>);
+                                        } else if (scopedFormData.questionType === 'multi-step-question') {
+                                            return(
+                                            <Fragment>
+                                                <ArrayInput source={getSource('multi-step-question')} label='This is a multi step question, add questions in the order you would like them to be'>
+                                                    <SimpleFormIterator>
+                                                        <SelectInput source="questionType" fullWidth={true} label="Choose the question type for this step" choices={[
+                                                        {id: 'yes-no', name: 'Yes or No'},
+                                                        {id: 'number', name: 'Number Input'},
+                                                        {id: 'photo', name: 'Photo Input'},
+                                                        {id: 'open-ended', name: 'Open Ended'},
+                                                        {id: 'additional-info', name: 'Additional Information'},
+                                                        ]}/>
+                                                        <TextInput source="questionText" fullWidth={true} label="Question Text"/>
+                                                        <BooleanInput
+                                                        source="isRequired"
+                                                        label="Required?"
+                                                        fullWidth={true}
+                                                    />
+                                                    <TextInput source="answer" fullwidth={true} label="User Answer"/>
+                                                </SimpleFormIterator>
+                                                </ArrayInput>
+                                            </Fragment>
+                                            )
+                                        }else if (scopedFormData.questionType === 'photo') {
+                                            return(
+                                            <Fragment>
+                                                <ArrayInput source={getSource('answer')}>
+                                                    <PhotoInput source='photo' label={"Photo"}/>
+                                                </ArrayInput>
+                                            </Fragment>)
+                                        }
+                                        else return 
+                                    }
+                                }}
+                            </FormDataConsumer>
                             <TextInput source="answer" fullwidth={true} label="User Answer"/>
                             <PhotoInput source='photo' />
                         </OrderedFormIterator>
